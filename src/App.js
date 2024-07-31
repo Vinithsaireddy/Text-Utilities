33fb30e5f58aaa4174b67b417aa517cb3194bd83
Vinithsaireddy <148205482+Vinithsaireddy@users.noreply.github.com>
2024-07-31 15:23:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React,{useState} from "react";
 import Navbar from "./componets/Navbar";
 import TextForm from "./componets/TextForm";
-import About from "./componets/About";
+// import About from "./componets/About";
 import Alert from "./componets/Alert";
 import Theme from "./componets/Theme";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 export default function App() {
   const [theme, setTheme] = useState('light');
@@ -32,15 +32,16 @@ export default function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Theme theme={theme} />
         <Navbar theme={theme} toggleTheme={toggleTheme} title="Text Utilities" />
         <Alert alert={alert} />
-        <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm theme={theme} heading="Enter your text below" showAlert={showAlert} />} />
-        </Routes>
-      </Router>
+        <TextForm theme={theme} heading="Enter your text below" showAlert={showAlert} />
+        {/* <Routes> */}
+          {/* <Route exact path="/about" element={<About />} /> */}
+          {/* <Route exact path="/" element={<TextForm theme={theme} heading="Enter your text below" showAlert={showAlert} />} /> */}
+        {/* </Routes> */}
+      {/* </Router> */}
     </>
   );
 }
